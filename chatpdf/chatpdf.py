0cd8fd5a7897d7d77cff8b7459bc2ae91460f69f
rionzhao <rionzhao@tencent.com>
2024-02-21 22:58:41 +0800

    add: rerank and vectorDB

--- chatpdf/chatpdf.py
+++ chatpdf/chatpdf.py
@@ -6,6 +6,11 @@ import torch
 from loguru import logger
 from langchain.text_splitter import LangchainSplitter
 from vectordb.chroma import ChromaDB
+from langchain_community.llms import Ollama
+from typing import List, Union
+from langchain.retrievers.document_compressors import FlashrankRerank
+from langchain.retrievers import ContextualCompressionRetriever
+
 
 class ChatPDF:
     def __init__(
@@ -14,7 +19,7 @@ class ChatPDF:
         generate_model_type: str = "auto",
         generate_model_name_or_path: str = "01-ai/Yi-6B-Chat",
         lora_model_name_or_path: str = None,
-        # corpus_files: Union[str, List[str]] = None,
+        corpus_files: Union[str, List[str]] = None,
         save_corpus_emb_dir: str = "./corpus_embs/",
         device: str = None,
         # int8: bool = False,
@@ -45,6 +50,82 @@ class ChatPDF:
         self.text_splitter = LangchainSplitter(chunk_size, chunk_overlap)
 
         # 向量数据库模型
-        # TODO 此处不能传递参数，因为docs在初始化的时候根本不会传入进去
-        self.sim_model = ChromaDB
+        self.vectorDB = ChromaDB(self.text_splitter, similarity_top_k)
+        if corpus_files:
+            self.vectorDB.add_corpus(corpus_files)
+
+        self.num_expand_context_chunk = num_expand_context_chunk
+        self.rerank_top_k = rerank_top_k
+
+        # 历史记录
+        self.history = []
+        self.enable_history = enable_history
+
+        # llm设置
+        # See: https://python.langchain.com/docs/integrations/llms/ollama
+        self.model = Ollama(model="mistral:latest")
+
+
+    # 获取关联信息的相关度，用于评判召回质量
+    def get_reranker_score(self, query: str, reference_results: List[str]) -> List:
+        if self.compression_retriever is None:
+            # 对reference_results创建临时向量数据库，进行评估
+            db = ChromaDB(self.text_splitter, self.similarity_top_k)
+            db.add_corpus(reference_results)
+
+            self.compression_retriever = ContextualCompressionRetriever(
+                base_compressor=FlashrankRerank(), base_retriever=db.get_retriever()
+            )
+
+        compressed_docs = self.compression_retriever.get_relevant_documents(query)
+        return [doc.metadata["id"] for doc in compressed_docs]
+    
+
+    # 从向量数据库中获取与query相关联的资料
+    def get_reference_results(self, query: str):
+        """
+        Get reference results.
+            1. Similarity model get similar chunks
+            2. Rerank similar chunks
+            3. Expand reference context chunk
+        :param query:
+        :return:
+        """
+
+        reference_results = []
+        # 1. 搜索向量数据库
+        vec_contents = self.vectorDB.similarity_search(query)
+        for doc in vec_contents:
+            reference_results.append(doc.page_content)
+        
+        # rerank: 对获取的资料进行评估，返回最符合query的topK个
+        if reference_results:
+            rerank_scores = self.get_reranker_score(query, reference_results)
+            logger.debug(f"rerank_scores: {rerank_scores}")
+
+            # 获取topK个符合条件的chunks
+            # 将reference_results, rerank_scores通过zip()进行组合，然后按照rerank进行降序排序，输出前k个数据
+            reference_results = [reference for reference, score in sorted(
+                    zip(reference_results, rerank_scores), key=lambda x: x[1], reverse=True)][:self.rerank_top_k]
+
+        return reference_results
+    
+
+    # TODO 一次询问
+    def predict(
+        self,
+        query: str,
+        max_length: int = 512,
+        context_len: int = 2048,
+        temperature: float = 0.7,
+    ):
+        pass
+
+        
+
+
+
+
+
+
 
--- chatpdf/vectordb/chroma.py
+++ chatpdf/vectordb/chroma.py
@@ -2,15 +2,58 @@ from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,
 )
 from langchain_community.vectorstores.chroma import Chroma
-from typing import Union, Dict, List
+from typing import Union, Dict, List, Optional, Any
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+
+
+# 常量
+DEFAULT_K = 4
 
 
 class ChromaDB:
-    def __init__(self, docs: str) -> None:
-        # TODO embedding作为入参输入
-        embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        self.db = Chroma.from_texts(docs, embedding_function)
+    def __init__(self, splitter, similarity_top_k: int = 20) -> None:
+        self.similarity_top_k = similarity_top_k
 
-    # 增加语料库
-    def add_corpus(self, corpus: Union[List[str], Dict[str, str]]):
-        self.db.add_texts(corpus)
+        # embedding function
+        # create the open-source embedding function
+        self.embedding_function = self.get_embedding_function()
+        self.splitter = splitter
+        self.db = None
+
+
+    # TODO 将model选择与device进行定制化
+    def get_embedding_function(self) -> HuggingFaceEmbeddings:
+        # Embedding Model
+        modelPath = "BAAI/bge-base-en-v1.5"
+
+        # Create a dictionary with model configuration options, specifying to use the CPU for computations
+        # model_kwargs = {'device':'cpu'}
+        model_kwargs = {'device':'mps'}
+
+        # Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
+        encode_kwargs = {'normalize_embeddings': True}
+
+        # Initialize an instance of HuggingFaceEmbeddings with the specified parameters
+        embeddings = HuggingFaceEmbeddings(
+            model_name=modelPath,     # Provide the pre-trained model's path
+            model_kwargs=model_kwargs, # Pass the model configuration options
+            encode_kwargs=encode_kwargs # Pass the encoding options
+        )
+        return embeddings
+
+
+    def add_corpus(self, files: Union[str, List[str]]):
+        '''加载文件'''
+        if isinstance(files, str):
+            files = [files]
+        
+        self.db = Chroma.from_documents(files, self.embedding_function)
+
+
+    def get_retriever(self):
+        return self.db.as_retriever(search_kwargs={"k": self.similarity_top_k})
+    
+    
+    def similarity_search(self, query: str) -> List[Document]:
+        return self.db.similarity_search(query, self.similarity_top_k)
